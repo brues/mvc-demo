0373ef9c3726c3447387a6dfa52b8f0714e525a2
guoyb <guoyb@anbi.tv>
2014-07-25 11:45:57 +0800

    权限类中增加方法：根据父id查询列表

--- one/one-modules/one-modules-basic/src/main/java/com/mvc/basic/dao/PurviewDao.java
+++ one/one-modules/one-modules-basic/src/main/java/com/mvc/basic/dao/PurviewDao.java
@@ -4,9 +4,14 @@ import com.mvc.basic.entity.Purview;
 import com.mvc.modules.core.BaseDao;
 import com.mvc.modules.core.annotation.MyBatisRepository;
 
+import java.util.List;
+
 /**
  * Created by guoyibin on 7/25/14.
  */
 @MyBatisRepository
 public interface PurviewDao extends BaseDao<Purview> {
+
+    //根据父id查询列表
+    public List<Purview> findByParentId(Long parentId);
 }
